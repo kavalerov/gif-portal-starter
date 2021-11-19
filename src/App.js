0fda3f8e3ffbce09b2955c1445fe1a11d3eb4183
Ivan Kavalerov <ivan@kavalerov.net>
2021-11-19 17:12:40 +0000

    inital page working

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
@@ -6,6 +7,34 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  /*
+   * Lets check if Phantom wallet is connected
+   * */
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom Wallet found!');
+        }
+      } else {
+        alert('Solana wallet not found!');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect( () => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []
+           );
+
   return (
     <div className="App">
       <div className="container">
